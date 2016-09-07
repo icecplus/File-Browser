97d70727a6323f8b7774fdc0b58d01854f8c9643
Akhmed <axmedterro@gmail.com>
2016-09-07 13:08:13 +0300

    File Browser

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.List = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnOpen.Location = new System.Drawing.Point(371, 5);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(88, 26);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // List
+            // 
+            this.List.LargeImageList = this.imageList;
+            this.List.Location = new System.Drawing.Point(15, 44);
+            this.List.MultiSelect = false;
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(444, 306);
+            this.List.Sorting = System.Windows.Forms.SortOrder.Descending;
+            this.List.TabIndex = 1;
+            this.List.UseCompatibleStateImageBehavior = false;
+            this.List.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Paint += new System.Windows.Forms.PaintEventHandler(this.label1_Paint);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(52, 9);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(313, 20);
+            this.txtPath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(483, 371);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.List);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "File Browser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView List;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        List<string> listfiles = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (List.FocusedItem != null)
+                Process.Start(listfiles[List.FocusedItem.Index]);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        { 
+            listfiles.Clear();
+            List.Items.Clear();
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = fbd.SelectedPath;
+                    foreach (string item in Directory.GetFiles(fbd.SelectedPath))
+                    {
+
+                        imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                        FileInfo fi = new FileInfo(item);
+                        listfiles.Add(fi.FullName);
+                        List.Items.Add(fi.Name, imageList.Images.Count - 1);
+                    }
+                }
+            }
+
+        }
+
+        private void label1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
